5aa62a3b98597256f19415157b9c76df81305d91
Zhaoheng Zheng <zhaohenz@dc3swp43.dc.umich.edu>
2017-09-17 17:36:46 -0400

    ready for submission

--- hw1/code/main.m
+++ hw1/code/main.m
@@ -5,11 +5,14 @@ k = [2 10 40];
 
 for i=1:3
     app_x = zeros(size(X));
+    sum_num = 0;
     for j = 1:k(i)
         app_x = app_x + S(j, j) * U(:, j) * V(:, j)';
+        sum_num = sum_num + 1 + size(U(:, j)) + size(V(:, j));
     end
     s = sprintf('%d_app.jpg', k(i));
     imwrite(app_x / 256, s);
-    error = norm(X - app_x, 'fro') / norm(X, 'fro');
-    fprintf('Top %d approximation, error: %f\n', k(i), error);
+    ratio = norm(X - app_x, 'fro') / norm(X, 'fro');
+    fprintf('Top %d approximation, error: %f\n', k(i), ratio);
+    fprintf('numbers to store: %d\n', sum_num(1));
 end
