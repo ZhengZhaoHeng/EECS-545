acd676bfcf15e6ca2c3c747ce687d5de8e84b16b
thu-zhengzh13 <zhengzh13.tsinghua@gmail.com>
2017-11-08 13:12:38 -0500

    fuck

--- hw3/svm.m
+++ hw3/svm.m
@@ -1,4 +1,4 @@
-rng(42);
+
 
 training_label = label(1:500, :);
 training_data = diabetesscale(1:500, :);
@@ -9,6 +9,7 @@ constant = linspace(0.1, 2, 20);
 losses = [];
 
 for i = 1 : 20
+    rng(42);
     model = fitcsvm(training_data, training_label, 'KernelFunction', 'linear', 'KernelScale', 1, 'BoxConstraint', constant(i), 'CrossVal', 'on', 'KFold', 5);
     loss = kfoldLoss(model);
     losses = [losses loss];
@@ -19,6 +20,6 @@ best_c_model = fitcsvm(training_data, training_label,  'KernelFunction', 'linear
 pred = predict(best_c_model, test_data);
 fprintf('Soft-Margin SVM Test Accuracy: %.2f%%\n', sum(pred==test_label) / 268 * 100);
 
-hard_margin_model = fitcsvm(training_data, training_label, 'KernelFunction', 'linear', 'KernelScale', 1, 'BoxConstraint', 1e5);
+hard_margin_model = fitcsvm(training_data, training_label, 'KernelFunction', 'linear', 'KernelScale', 1, 'BoxConstraint', 1e6);
 pred = predict(hard_margin_model, test_data);
 fprintf('Hard-Margin SVM Test Accuracy: %.2f%%\n', sum(pred==test_label) / 268 * 100);
